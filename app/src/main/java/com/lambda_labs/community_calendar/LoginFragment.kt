c5437c6c6009c2052a7634823c1e2185cff0f720
Tyler Berrett <btylerberrett@gmail.com>
2019-12-19 13:42:22 -0700

    add Auth0 login functionality in LoginFragment
    Co-authored-by: justinbgent <justinbgent@gmail.com>
    Co-authored-by: heiligbasil <basilief@yahoo.com>

--- app/src/main/java/com/lambda_labs/community_calendar/LoginFragment.kt
+++ app/src/main/java/com/lambda_labs/community_calendar/LoginFragment.kt
@@ -1,22 +1,57 @@
 package com.lambda_labs.community_calendar
 
+import android.app.Activity
+import android.app.Dialog
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.provider.AuthCallback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
 
 
 class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    lateinit var auth0: Auth0
+    lateinit var mainActivity: Activity
 
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as Activity
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth0 = Auth0(mainActivity)
+        auth0.isOIDCConformant = true
+
+
+        WebAuthProvider.login(auth0)
+            .withScheme("demo")
+            .withAudience(getString(R.string.audience))
+            .start(mainActivity, object : AuthCallback {
+                override fun onSuccess(credentials: Credentials) {
+                    println(credentials.accessToken)
+                }
+
+                override fun onFailure(dialog: Dialog) {
+                    println("Fail Dialog")
+                }
+
+                override fun onFailure(exception: AuthenticationException?) {
+                    println(exception?.message)
+                }
+
+            })
+
 
     }
 
