9f007c52282c6facaecf733987612440266e60ca
Tyler Berrett <btylerberrett@gmail.com>
2020-01-21 17:24:24 -0700

    add functionality of display filters about recent search in a drop down add getRecentSearchDisplay to format date display date like MM-DD-YYYY change negativeDate() to return just March 12 1998 without time

--- app/src/main/java/com/lambda_labs/community_calendar/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/lambda_labs/community_calendar/viewmodel/SearchViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.lambda_labs.community_calendar.App
 import com.lambda_labs.community_calendar.model.Search
+import com.lambda_labs.community_calendar.util.negativeDate
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -31,6 +32,30 @@ class SearchViewModel: ViewModel() {
 
     fun removeRecentSearch(search: Search){
         App.repository.removeRecentSearch(search)
+    }
+
+    /* Checks filters to see if the user changed any then adds them
+    to a list which is used in RecentSearchRecycler */
+    fun searchToSearchList(search: Search): ArrayList<Any> {
+        val list = ArrayList<Any>()
+        val location = search.location
+        if (location.isNotEmpty()) list.add(location)
+        val zipcode = search.zipcode
+        if (zipcode != -1) list.add(zipcode)
+        val date = search.date
+        if (date != negativeDate()) list.add(date)
+        val tags = search.tags
+        if (tags[0].isNotEmpty()) list.add(tags)
+
+        return list
+    }
+    // Checks search to see if any filters have been added
+    fun hasNoFilters(search: Search): Boolean {
+        val checkLocation = search.location.isEmpty()
+        val checkZipcode = search.zipcode == -1
+        val checkDate = search.date == negativeDate()
+        val checkTags = search.tags[0].isEmpty()
+        return checkLocation && checkZipcode && checkDate && checkTags
     }
 
     override fun onCleared() {
